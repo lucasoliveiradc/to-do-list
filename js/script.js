e4694e5734203c037498d7f88b672f47f7f3b8cb
Lucas Oliveira <lucasdthe@gmail.com>
2024-05-27 00:15:36 -0300

    button personalizado

--- js/script.js
+++ js/script.js
@@ -7,21 +7,26 @@ function adicionar(){
     if(tarefa.value.length != 0){
 
         lista.push(tarefa.value);
+        //Checkbox
         let check = document.createElement("input");
         check.setAttribute("type", "checkbox");
         result.appendChild(check);
-
+        //Input text
         let tarefas = document.createElement('input');
         tarefas.setAttribute('type', 'text');
         tarefas.setAttribute('value', tarefa.value);
         tarefas.innerText = `${tarefa.value}`;
         tarefas.classList.add("list"); 
         result.appendChild(tarefas);
-
+        //button caso queira apagar o todo
         let button = document.createElement('button');
-        let clear = document.createTextNode("Apagar");
-        button.appendChild(clear);
+        button.setAttribute("type", "button")
+        let clear = document.createTextNode("");
+        let img = document.createElement("img");
+        img.src = "imgs/trash-can.png";
+        button.appendChild(img);
         result.appendChild(button);
+
 
         if(tarefa.value != ""){
             tarefa.value = "";
